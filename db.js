a1207a1e3cb5751ce1251355c513c9cd1274ff07
othman <othman@gmail.com>
2022-03-21 18:17:31 +0100

    final push

--- db.js
+++ db.js
@@ -1,8 +1,16 @@
 var unrest = require("unirest")
-var req = unrest ("GET", "http://localhost:3000/data")
+var req = unrest ("GET", "http://localhost:3000/data?_limit=10")
+var req1 = unrest("GET","http://localhost:3000/data");
 req.headers({
 "cache-control": "no-cache",
 })
 req. end(function (res){ 
 if (res.error) throw new Error(res.error)
-console.log(res.body)})
+console.log(res.body)});
+
+
+req1.end(function (res) {
+  if (res.error) throw new Error(res,error)
+  console.log("ddedededededfk vz vozjv nk vo gzr joiz hf z v h h nv h lboedededededede")
+  console.log(res.body.filter((data) => data.attributes.title.en.startsWith("m")))
+})
