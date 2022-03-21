afcc80d77953770f3873d268c38441231dc15b83
othman <othman@gmail.com>
2022-03-21 18:26:41 +0100

    push

--- db.js
+++ db.js
@@ -11,6 +11,6 @@ console.log(res.body)});
 
 req1.end(function (res) {
   if (res.error) throw new Error(res,error)
-  console.log("ddedededededfk vz vozjv nk vo gzr joiz hf z v h h nv h lboedededededede")
+
   console.log(res.body.filter((data) => data.attributes.title.en.startsWith("m")))
 })
